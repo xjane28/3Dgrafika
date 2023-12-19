4d471e3ea54673af9894b31b1e57f337ee5c3f99
xjane28 <136444348+xjane28@users.noreply.github.com>
2023-12-19 13:20:41 +0100

    Update geometry.h

--- Zadaca2/geometry.h
+++ Zadaca2/geometry.h
@@ -4,17 +4,24 @@
 #include <cassert>
 #include <iostream>
 
+
 // main template with parameters dimension and type 
 template <size_t DIM, typename T> struct vec // defining a vector
 {
+
     // constructor
     vec() { for (size_t i=DIM; i--; data_[i] = T()); }
+
+
     // access constructor
     T& operator[](const size_t i)             {    assert(i<DIM); return data_[i]; }
     const T& operator[](const size_t i) const {    assert(i<DIM); return data_[i]; }
+
 private:
     T data_[DIM];
+
 };
+
 
 // type specialization depending on float or int and dimension size 
 typedef vec <2, float> Vec2f;
@@ -22,47 +29,64 @@ typedef vec <3, float> Vec3f;
 typedef vec <3, int  > Vec3d;
 typedef vec <4, float> Vec4f;
 
+
 template <typename T> struct vec<2,T> 
 {
     // constructors
     vec() : x(T()), y(T()) {}
     vec(T X, T Y) : x(X), y(Y) {}
     template <class U> vec<2,T>(const vec<2,U> &v);
+
+        
     // access constructor
     T& operator[](const size_t i)       { assert(i<2); return i<=0 ? x : y; }
     const T& operator[](const size_t i) const { assert(i<2); return i<=0 ? x : y; }
+
+        
     // components
     T x,y;
 };
 
-template <typename T> struct vec<3,T> {
+template <typename T> struct vec<3,T> 
+{
+
     // constructors
     vec() : x(T()), y(T()), z(T()) {}
     vec(T X, T Y, T Z) : x(X), y(Y), z(Z) {}
+
+
     // access constructors
     T& operator[]      (const size_t i)       { assert(i<3); return i<=0 ? x : (1==i ? y : z); }
     const T& operator[](const size_t i) const { assert(i<3); return i<=0 ? x : (1==i ? y : z); }
+
 
     // normalization
     float norm()                     { return std::sqrt(x*x+y*y+z*z); }    
     float norm()  const              { return std::sqrt(x*x+y*y+z*z); }    
     vec<3,T> & normalize(T l=1) { *this = (*this)*(l/norm()); return *this; }
+
+
     // components
     T x,y,z;
 };
 
 template <typename T> struct vec<4,T> 
 {
+
     // constructors
     vec() : x(T()), y(T()), z(T()), w(T()) {}
     vec(T X, T Y, T Z, T W) : x(X), y(Y), z(Z), w(W) {}
+
+
     // access constructor
     T& operator[]      (const size_t i)       { assert(i<4); return i<=0 ? x : (1==i ? y : (2==i ? z : w)); }
     const T& operator[](const size_t i) const { assert(i<4); return i<=0 ? x : (1==i ? y : (2==i ? z : w)); }
 
+
     // components
     T x,y,z,w;
 };
+
 
 // dot product implementation (skalarni produt)
 template<size_t DIM,typename T> T operator*(const vec<DIM,T>& lhs, const vec<DIM,T>& rhs) {
@@ -72,40 +96,74 @@ template<size_t DIM,typename T> T operator*(const vec<DIM,T>& lhs, const vec<DIM
 }
 
 // vector addition implementation
-template<size_t DIM,typename T>vec<DIM,T> operator+(vec<DIM,T> lhs, const vec<DIM,T>& rhs) {
+template<size_t DIM,typename T>vec<DIM,T> operator+(vec<DIM,T> lhs, const vec<DIM,T>& rhs)
+{
+    
     for (size_t i=DIM; i--; lhs[i]+=rhs[i]);
     return lhs;
+
 }
+
+
 // vector subtraction rhs
-template<size_t DIM,typename T>vec<DIM,T> operator-(vec<DIM,T> lhs, const vec<DIM,T>& rhs) {
+template<size_t DIM,typename T>vec<DIM,T> operator-(vec<DIM,T> lhs, const vec<DIM,T>& rhs)
+{
+    
     for (size_t i=DIM; i--; lhs[i]-=rhs[i]);
     return lhs;
+    
 }
+
+
 // product by components implementation
-template<size_t DIM,typename T,typename U> vec<DIM,T> operator*(const vec<DIM,T> &lhs, const U& rhs) {
+template<size_t DIM,typename T,typename U> vec<DIM,T> operator*(const vec<DIM,T> &lhs, const U& rhs)
+{
+    
     vec<DIM,T> ret;
     for (size_t i=DIM; i--; ret[i]=lhs[i]*rhs);
     return ret;
+    
 }
+
+
 // vector subtraction implementation lhs
-template<size_t DIM,typename T> vec<DIM,T> operator-(const vec<DIM,T> &lhs) {
+template<size_t DIM,typename T> vec<DIM,T> operator-(const vec<DIM,T> &lhs) 
+
+{
     return lhs*T(-1);
+    
 }
+
 
 // vector product
-template <typename T> vec<3,T> cross(vec<3,T> v1, vec<3,T> v2) {
+template <typename T> vec<3,T> cross(vec<3,T> v1, vec<3,T> v2)
+{
+    
     return vec<3,T>(v1.y*v2.z - v1.z*v2.y, v1.z*v2.x - v1.x*v2.z, v1.x*v2.y - v1.y*v2.x);
+    
 }
+
 
 // 3x3 matrix determinant
-template <typename T> float determinant(const vec<3,T> &v1,const vec<3,T> &v2, const vec<3,T> &v3){
+template <typename T> float determinant(const vec<3,T> &v1,const vec<3,T> &v2, const vec<3,T> &v3)
+{
+    
     return (v1.x * (v2.y*v3.z-v2.z*v3.y ) - v2.x * (v1.y*v3.z-v1.z*v3.y ) + v3.x * (v1.y*v2.z-v1.z*v2.y ));
+    
 }
 
+
 // printing a vector
-template <size_t DIM, typename T> std::ostream& operator<<(std::ostream& out, const vec<DIM,T>& v) {
-    for(unsigned int i=0; i<DIM; i++) {
+template <size_t DIM, typename T> std::ostream& operator<<(std::ostream& out, const vec<DIM,T>& v) 
+{
+    
+    for(unsigned int i=0; i<DIM; i++)
+    {
+        
         out << v[i] << " " ;
+        
     }
+
+    
     return out ;
 }
